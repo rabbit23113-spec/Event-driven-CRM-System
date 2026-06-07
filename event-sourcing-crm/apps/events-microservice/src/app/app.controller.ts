794b530df01fb541efcc37847b9b73d698f2917c
Raffinnaisse <rabbit23113@gmail.com>
2026-06-07 13:23:03 +0300

    users microservice: users in api gateway

--- event-sourcing-crm/apps/events-microservice/src/app/app.controller.ts
+++ event-sourcing-crm/apps/events-microservice/src/app/app.controller.ts
@@ -8,17 +8,17 @@ export class AppController {
   constructor(private readonly appService: AppService) {
   }
 
-  @MessagePattern({ cmd: "events.microservice: findAll" })
+  @MessagePattern({cmd: "events.microservice: findAll"})
   async findAll() {
     return await this.appService.findAll();
   }
 
-  @MessagePattern({ cmd: "events.microservice: findOne" })
+  @MessagePattern({cmd: "events.microservice: findOne"})
   async findOne(@Payload() payload: { id: string }) {
     return await this.appService.findOne(payload.id);
   }
 
-  @MessagePattern({ cmd: "events.microservice: createOne" })
+  @MessagePattern({cmd: "events.microservice: createOne"})
   async createOne(@Payload() payload: { dto: CreateEventDto }) {
     return await this.appService.createOne(payload.dto)
   }
