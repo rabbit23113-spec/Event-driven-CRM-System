6762d4365291d7225df27ba6d64c338243603477
Raffinnaisse <rabbit23113@gmail.com>
2026-06-07 13:13:13 +0300

    users microservice: jest tests

--- event-sourcing-crm/apps/users-microservice/src/app/app.controller.spec.ts
+++ event-sourcing-crm/apps/users-microservice/src/app/app.controller.spec.ts
@@ -1,21 +1,37 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {CreateUserDto} from "./dto/create-user.dto";
 
 describe('AppController', () => {
   let app: TestingModule;
+  const appServiceMock = {
+    createUser: jest.fn(),
+  }
 
   beforeAll(async () => {
     app = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [AppService, {
+        provide: AppService,
+        useValue: appServiceMock
+      }],
     }).compile();
   });
 
-  describe('getData', () => {
-    it('should return "Hello API"', () => {
+  describe('createUser', () => {
+    it('should return a new user from the database', async () => {
       const appController = app.get<AppController>(AppController);
-      expect(appController.getData()).toEqual({ message: 'Hello API' });
+      const dto: CreateUserDto = {
+        firstName: "Alex",
+        lastName: "Cox",
+        email: "alexcox@example.com",
+        password: "123456"
+      }
+      appServiceMock.createUser.mockResolvedValue(dto)
+      const newUser = await appController.createUser({dto});
+      expect(newUser).toMatchObject(dto);
+      expect(appServiceMock.createUser).toHaveBeenCalledWith(dto)
     });
   });
-});
+})
--- event-sourcing-crm/apps/users-microservice/src/app/app.service.spec.ts
+++ event-sourcing-crm/apps/users-microservice/src/app/app.service.spec.ts
@@ -1,20 +1,39 @@
 import { Test } from '@nestjs/testing';
 import { AppService } from './app.service';
+import {CreateUserDto} from "./dto/create-user.dto";
+import {UserEntity} from "./entities/user.entity";
+import {getRepositoryToken} from "@nestjs/typeorm";
 
 describe('AppService', () => {
   let service: AppService;
+  const mockRepo = {
+    create: jest.fn(),
+    save: jest.fn(),
+  }
 
   beforeAll(async () => {
     const app = await Test.createTestingModule({
-      providers: [AppService],
+      providers: [AppService, {
+        provide: getRepositoryToken(UserEntity),
+        useValue: mockRepo
+      }],
     }).compile();
 
     service = app.get<AppService>(AppService);
   });
 
-  describe('getData', () => {
-    it('should return "Hello API"', () => {
-      expect(service.getData()).toEqual({ message: 'Hello API' });
+  describe('createOne', () => {
+    it('should return a new user from the database', async () => {
+      const dto: CreateUserDto = {
+        firstName: "Alex",
+        lastName: "Cox",
+        email: "alexcox@example.com",
+        password: "123456"
+      }
+      mockRepo.create.mockReturnValue(dto);
+      mockRepo.save.mockResolvedValue(dto)
+      const newUser = await service.createUser(dto);
+      expect(newUser).toMatchObject(dto);
     });
   });
 });
