90157ee7eaad64d870da053da40e599da645ccfe
Raffinnaisse <rabbit23113@gmail.com>
2026-06-07 11:45:53 +0300

    events microservice: fixes in controller and service

--- event-sourcing-crm/apps/api-gateway/src/app/events/events.controller.ts
+++ event-sourcing-crm/apps/api-gateway/src/app/events/events.controller.ts
@@ -1,48 +1,35 @@
 import {Body, Controller, Get, Inject, Post, Req} from '@nestjs/common';
-import { EventsService } from './events.service';
-import type { Request } from "express"
+import {EventsService} from './events.service';
+import type {Request} from "express"
 import {EventDto} from "../dto/events/event.dto";
 import {CreateEventDto} from "../dto/events/create-event.dto";
 import {ApiOperation, ApiQuery, ApiResponse} from "@nestjs/swagger";
-import {CACHE_MANAGER} from "@nestjs/cache-manager";
-import type {Cache} from "cache-manager";
 
 
 @Controller('events')
 export class EventsController {
-  constructor(private readonly eventsService: EventsService, @Inject(CACHE_MANAGER) private readonly cache: Cache) {}
+  constructor(private readonly eventsService: EventsService) {
+  }
 
-  @ApiOperation({ summary: "Get all events or one by id" })
+  @ApiOperation({summary: "Get all events or one by id"})
   @ApiQuery({
     name: 'id',
     type: 'string',
     required: false,
   })
-  @ApiResponse({ status: 200, type: EventDto, isArray: true })
+  @ApiResponse({status: 200, type: EventDto, isArray: true})
   @Get("find")
   async findEvents(@Req() req: Request): Promise<EventDto[] | EventDto> {
     const id = req.query.id;
     if (req.query.id) {
-      const event: string | undefined = await this.cache.get(`events:${id}`)
-      if (event) {
-        return JSON.parse(event)
-      }
-      const result = await this.eventsService.findOne(String(id))
-      await this.cache.set(`events:${id}`, JSON.stringify(result))
-      return result
+      return await this.eventsService.findOne(String(id))
     } else {
-      const events: string | undefined = await this.cache.get(`events:all`)
-      if (events) {
-        return JSON.parse(events)
-      }
-      const result =  await this.eventsService.findAll();
-      await this.cache.set(`events:all`, JSON.stringify(result))
-      return result
+      return await this.eventsService.findAll();
     }
   }
 
-  @ApiOperation({ summary: "Create event" })
-  @ApiResponse({ status: 201, type: EventDto })
+  @ApiOperation({summary: "Create event"})
+  @ApiResponse({status: 201, type: EventDto})
   @Post("create")
   async createOne(@Body() body: CreateEventDto): Promise<EventDto> {
     return await this.eventsService.createOne(body);
--- event-sourcing-crm/apps/api-gateway/src/app/events/events.service.ts
+++ event-sourcing-crm/apps/api-gateway/src/app/events/events.service.ts
@@ -4,18 +4,32 @@ import {RMQ_EVENTS_CLIENT_ID} from "../constants/constants";
 import {firstValueFrom} from "rxjs";
 import {EventDto} from "../dto/events/event.dto";
 import {CreateEventDto} from "../dto/events/create-event.dto";
+import {CACHE_MANAGER} from "@nestjs/cache-manager";
+import type {Cache} from "cache-manager";
 
 @Injectable()
 export class EventsService {
-  constructor(@Inject(RMQ_EVENTS_CLIENT_ID) private readonly client: ClientProxy) {
+  constructor(@Inject(RMQ_EVENTS_CLIENT_ID) private readonly client: ClientProxy, @Inject(CACHE_MANAGER) private readonly cache: Cache) {
   }
 
   async findAll(): Promise<EventDto[]> {
-    return await firstValueFrom(this.client.send({cmd: "events.microservice: findAll"}, {}))
+    const result: EventDto[] | undefined = await this.cache.get("events:all")
+    if (result) {
+      return result
+    }
+    const response: EventDto[] = await firstValueFrom(this.client.send({cmd: "events.microservice: findAll"}, {}))
+    await this.cache.set("events:all", response)
+    return response
   }
 
   async findOne(id: string): Promise<EventDto> {
-    return await firstValueFrom(this.client.send({cmd: "events.microservice: findOne"}, {id}))
+    const result: EventDto | undefined = await this.cache.get(`events:${id}`)
+    if (result) {
+      return result
+    }
+    const response: EventDto = await firstValueFrom(this.client.send({cmd: "events.microservice: findOne"}, {id}))
+    await this.cache.set(`events:${id}`, response)
+    return response
   }
 
   async createOne(dto: CreateEventDto): Promise<EventDto> {
