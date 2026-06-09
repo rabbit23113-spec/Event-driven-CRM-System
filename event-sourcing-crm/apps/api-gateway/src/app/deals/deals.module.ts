b7fe134efcaf15df71be3a862b364a0533573f3d
Raffinnaisse <rabbit23113@gmail.com>
2026-06-09 15:25:46 +0300

    deals microservice: service in api gateway

--- event-sourcing-crm/apps/api-gateway/src/app/deals/deals.module.ts
+++ event-sourcing-crm/apps/api-gateway/src/app/deals/deals.module.ts
@@ -1,9 +1,33 @@
-import { Module } from '@nestjs/common';
-import { DealsService } from './deals.service';
-import { DealsController } from './deals.controller';
+import {Module} from '@nestjs/common';
+import {DealsService} from './deals.service';
+import {DealsController} from './deals.controller';
+import {ClientsModule, Transport} from "@nestjs/microservices";
+import * as constants from "../constants/constants";
+import {CacheModule} from "@nestjs/cache-manager";
+import {redisStore} from "cache-manager-redis-store";
 
 @Module({
+  imports: [
+    ClientsModule.register([{
+      name: constants.RMQ_DEALS_CLIENT_ID,
+      transport: Transport.RMQ,
+      options: {
+        urls: [`amqp://${constants.RMQ_USER}:${constants.RMQ_PASS}@${constants.RMQ_HOST}:${constants.RMQ_PORT}`],
+        queue: constants.RMQ_DEALS_QUEUE,
+        queueOptions: {
+          durable: true,
+        },
+      }
+    }]),
+    CacheModule.register({
+      store: redisStore,
+      host: constants.REDIS_HOST,
+      port: constants.REDIS_PORT,
+      ttl: 60,
+    }),
+  ],
   controllers: [DealsController],
   providers: [DealsService],
 })
-export class DealsModule {}
+export class DealsModule {
+}
--- event-sourcing-crm/apps/api-gateway/src/app/deals/deals.service.ts
+++ event-sourcing-crm/apps/api-gateway/src/app/deals/deals.service.ts
@@ -1,4 +1,57 @@
-import { Injectable } from '@nestjs/common';
+import {Inject, Injectable} from '@nestjs/common';
+import {RMQ_DEALS_CLIENT_ID} from "../constants/constants";
+import {ClientProxy} from "@nestjs/microservices";
+import {CACHE_MANAGER} from "@nestjs/cache-manager";
+import type {Cache} from "cache-manager";
+import {firstValueFrom} from "rxjs";
+import {CreateDealDto} from "../dto/deals/create-deal.dto";
+import {DealDto, Status} from "../dto/deals/deal.dto";
+import {UpdateDealDto} from "../dto/deals/update-deal.dto";
 
 @Injectable()
-export class DealsService {}
+export class DealsService {
+  constructor(@Inject(RMQ_DEALS_CLIENT_ID) private readonly client: ClientProxy, @Inject(CACHE_MANAGER) private readonly cache: Cache) {
+  }
+
+  async findAll(): Promise<DealDto[]> {
+    const cachedLeads: DealDto[] | undefined = await this.cache.get("deals:all")
+    if (cachedLeads) {
+      return cachedLeads;
+    }
+    const deals = await firstValueFrom(this.client.send({cmd: "deals.microservice: findAll"}, {}))
+    await this.cache.set("deals:all", deals);
+    return deals;
+  }
+
+  async findOne(id: string): Promise<DealDto> {
+    const cachedLead: DealDto | undefined = await this.cache.get(`deals:${id}`);
+    if (cachedLead) {
+      return cachedLead
+    }
+    const lead = await firstValueFrom(this.client.send({cmd: "deals.microservice: findOne"}, {id}))
+    await this.cache.set(`deals:${id}`, lead);
+    return lead
+  }
+
+  async findByStatus(status: Status): Promise<DealDto[]> {
+    const cachedLeads: DealDto[] | undefined = await this.cache.get(`deals:${status}`);
+    if (cachedLeads) {
+      return cachedLeads;
+    }
+    const deals = await firstValueFrom(this.client.send({cmd: "deals.microservice: findByStatus"}, {status}))
+    await this.cache.set(`deals:${status}`, deals);
+    return deals
+  }
+
+  async createOne(dto: CreateDealDto): Promise<DealDto> {
+    return await firstValueFrom(this.client.send({cmd: "deals.microservice: createOne"}, {dto}))
+  }
+
+  async updateOne(dto: UpdateDealDto): Promise<void> {
+    return await firstValueFrom(this.client.emit({cmd: "deals.microservice: updateOne"}, {dto}))
+  }
+
+  async deleteOne(id: string): Promise<void> {
+    return await firstValueFrom(this.client.emit({cmd: "deals.microservice: deleteOne"}, {id}))
+  }
+}
