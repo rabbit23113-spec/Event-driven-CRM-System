9c726720b8bd045e1097552a990a311e8f3816d6
Raffinnaisse <rabbit23113@gmail.com>
2026-06-09 11:10:24 +0300

    clients microservice: fixes in api gateway

--- event-sourcing-crm/apps/api-gateway/src/app/clients/clients.module.ts
+++ event-sourcing-crm/apps/api-gateway/src/app/clients/clients.module.ts
@@ -1,8 +1,32 @@
 import { Module } from '@nestjs/common';
 import { ClientsService } from './clients.service';
 import { ClientsController } from './clients.controller';
+import * as constants from "../constants/constants";
+import {Transport} from "@nestjs/microservices";
+import {CacheModule} from "@nestjs/cache-manager";
+import {redisStore} from "cache-manager-redis-store";
+import { ClientsModule as cm} from "@nestjs/microservices";
 
 @Module({
+  imports: [
+    cm.register([{
+      name: constants.RMQ_CLIENTS_CLIENT_ID,
+      transport: Transport.RMQ,
+      options: {
+        urls: [`amqp://${constants.RMQ_USER}:${constants.RMQ_PASS}@${constants.RMQ_HOST}:${constants.RMQ_PORT}`],
+        queue: constants.RMQ_CLIENTS_QUEUE,
+        queueOptions: {
+          durable: true,
+        },
+      }
+    }]),
+    CacheModule.register({
+      store: redisStore,
+      host: constants.REDIS_HOST,
+      port: constants.REDIS_PORT,
+      ttl: 60,
+    }),
+  ],
   controllers: [ClientsController],
   providers: [ClientsService],
 })
