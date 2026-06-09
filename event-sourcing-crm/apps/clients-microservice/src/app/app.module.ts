5b71842c6c24930505be665afbf67314e499343d
Raffinnaisse <rabbit23113@gmail.com>
2026-06-09 10:35:59 +0300

    clients microservice: service

--- event-sourcing-crm/apps/clients-microservice/src/app/app.module.ts
+++ event-sourcing-crm/apps/clients-microservice/src/app/app.module.ts
@@ -1,9 +1,35 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {ClientsModule, Transport} from "@nestjs/microservices";
+import * as constants from "./constants/constants"
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {ClientEntity} from "./entities/client.entity";
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: constants.DATABASE_TYPE,
+    host: constants.DATABASE_HOST,
+    port: constants.DATABASE_PORT,
+    username: constants.DATABASE_USER,
+    password: constants.DATABASE_PASSWORD,
+    database: constants.DATABASE_NAME,
+    synchronize: true,
+    entities: [ClientEntity]
+  }),
+    TypeOrmModule.forFeature([ClientEntity]),
+    ClientsModule.register([{
+      name: constants.RMQ_EVENTS_CLIENT_ID,
+      transport: Transport.RMQ,
+      options: {
+        urls: [`amqp://${constants.RMQ_USER}:${constants.RMQ_PASS}@${constants.RMQ_HOST}:${constants.RMQ_PORT}`],
+        queue: constants.RMQ_EVENTS_QUEUE,
+        queueOptions: {
+          durable: true,
+        },
+      }
+    }]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- event-sourcing-crm/apps/clients-microservice/src/app/app.service.ts
+++ event-sourcing-crm/apps/clients-microservice/src/app/app.service.ts
@@ -1,8 +1,64 @@
-import { Injectable } from '@nestjs/common';
+import {Inject, Injectable, NotFoundException} from '@nestjs/common';
+import {ClientProxy} from "@nestjs/microservices";
+import {ClientEntity} from "./entities/client.entity";
+import {RMQ_EVENTS_CLIENT_ID} from "./constants/constants";
+import {InjectRepository} from "@nestjs/typeorm";
+import {Repository} from "typeorm";
+import {CreateClientDto} from "./dto/create-client.dto";
+import {UpdateClientDto} from "./dto/update-client.dto";
 
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
-    return { message: 'Hello API' };
+  constructor(@InjectRepository(ClientEntity) private readonly leadRepo: Repository<ClientEntity>, @Inject(RMQ_EVENTS_CLIENT_ID) private readonly eventsClient: ClientProxy) {
+  }
+
+  async findAll(): Promise<ClientEntity[]> {
+    return await this.leadRepo.find();
+  }
+
+  async findOne(id: string): Promise<ClientEntity> {
+    const target = await this.leadRepo.findOneBy({id})
+    if (!target) {
+      throw new NotFoundException(`ClientEntity with id ${id} not found`);
+    }
+    return target;
+  }
+
+  async findByCompanyName(companyName: string): Promise<ClientEntity[]> {
+    return await this.leadRepo.findBy({companyName});
+  }
+
+  async findOneByName(name: string): Promise<ClientEntity> {
+    const target = await this.leadRepo.findOneBy({name});
+    if (!target) {
+      throw new NotFoundException(`ClientEntity with name ${name} not found`);
+    }
+    return target;
+  }
+
+  async createOne(dto: CreateClientDto): Promise<ClientEntity> {
+    const lead = await this.leadRepo.create(dto);
+    await this.leadRepo.save(lead);
+    this.eventsClient.send({ cmd: 'events.microservice: createOne' }, { domain: "client", action: "created", actorId: dto.ownerId, subjectId: lead.id })
+    return lead;
+  }
+
+  async updateOne(dto: UpdateClientDto): Promise<void> {
+    const {id, name, email, phone, companyName, ownerId} = dto;
+    const target = await this.findOne(id)
+    if (!target) {
+      throw new NotFoundException(`ClientEntity with id ${id} not found`);
+    }
+    this.eventsClient.send({ cmd: 'events.microservice: createOne' }, { domain: "client", action: "updated", actorId: dto.ownerId, subjectId: target.id })
+    await this.leadRepo.update(id, {name, email, phone, companyName, ownerId});
+  }
+
+  async deleteOne(id: string): Promise<void> {
+    const target = await this.findOne(id);
+    if (!target) {
+      throw new NotFoundException(`ClientEntity with id ${id} not found`);
+    }
+    this.eventsClient.send({ cmd: 'events.microservice: createOne' }, { domain: "client", action: "deleted", actorId: target.ownerId, subjectId: target.id })
+    await this.leadRepo.delete(id);
   }
 }
