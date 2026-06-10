fb5ef7e4fab9cef2e4ba2f682fbf79f6a7a01cbc
Raffinnaisse <rabbit23113@gmail.com>
2026-06-10 12:10:25 +0300

    tasks microservice: initial version

---
+++ event-sourcing-crm/apps/tasks-microservice/src/app/constants/constants.example.ts
@@ -0,0 +1,12 @@
+export const DATABASE_TYPE = "";
+export const DATABASE_USER = ""
+export const DATABASE_PASSWORD = ""
+export const DATABASE_HOST = "";
+export const DATABASE_PORT = 5432;
+export const DATABASE_NAME = ""
+
+export const RMQ_QUEUE = ""
+export const RMQ_USER = ""
+export const RMQ_PASS = ""
+export const RMQ_CONTAINER = ""
+export const RMQ_PORT = ""
