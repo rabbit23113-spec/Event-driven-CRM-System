6fb56271168c58dc0d50bdb0c99055d0d03fbad9
Raffinnaisse <rabbit23113@gmail.com>
2026-06-13 12:44:17 +0300

    refactoring: clean up e2e

--- event-sourcing-crm/apps/api-gateway-e2e/src/support/test-setup.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-import axios from 'axios';
-
-module.exports = async function () {
-  // Configure axios for tests to use.
-  const host = "localhost";
-  const port = "13050";
-  axios.defaults.baseURL = `http://${host}:${port}`;
-};
--- event-sourcing-crm/apps/api-gateway/src/app/apps/leads/src/leads.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class LeadsService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-}
