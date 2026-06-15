276d71b771e6ed68be2524ce07a9c2df8fe82f6d
Raffinnaisse <rabbit23113@gmail.com>
2026-06-15 22:55:32 +0300

    frontend: fixes in footer

--- frontend/app/layout/footer/footer.tsx
+++ frontend/app/layout/footer/footer.tsx
@@ -4,53 +4,38 @@ import Link from "next/link";
 
 export const Footer = () => {
     return (
-        <footer
-            className={`py-6 bg-background border border-border text-foreground flex items-center px-6 bottom-0 w-full`}>
-            <div className={"flex items-center w-full justify-between"}>
-                <img className={"w-13 h-13"} src={"../../favicon.ico"}/>
-                <nav className={"flex items-start justify-center gap-4"}>
-                    <div className={"flex items-center flex-col w-full justify-between gap-1.5"}>
-                        <div className={"text-foreground text-sm font-medium outline-0"}>Основное</div>
-                        <div className={"flex flex-col gap-1"}>
-                            <Link className={"text-foreground-secondary text-xs font-medium hover:text-foreground"}
-                                  href={"/"}>Главная</Link>
-                            <Link className={"text-foreground-secondary text-xs font-medium hover:text-foreground"}
-                                  href={"/dashboard"}>Дашборд</Link>
-                            <Link className={"text-foreground-secondary text-xs font-medium hover:text-foreground"}
-                                  href={"/audit"}>Журнал аудита</Link>
-                        </div>
+        <footer className={"h-50 w-full border border-border flex items-center justify-between px-24"}>
+            <div className={"text-foreground font-medium text-3.5"}>
+                CRM v0.1
+            </div>
+            <div className={"flex items-start justify-between gap-8"}>
+                <div className={"flex items-start justify-center gap-3.5 flex-col"}>
+                    <div className={"text-foreground text-3.5 font-medium"}>Основное</div>
+                    <div className={"flex flex-col gap-2"}>
+                        <Link className={"text-[14px] text-foreground-secondary hover:text-foreground"} href={"/"}>Главная</Link>
+                        <Link className={"text-[14px] text-foreground-secondary hover:text-foreground"} href={"/"}>Дашборд</Link>
+                        <Link className={"text-[14px] text-foreground-secondary hover:text-foreground"} href={"/"}>Журнал аудита</Link>
+                        <Link className={"text-[14px] text-foreground-secondary hover:text-foreground"} href={"/"}>Настройки</Link>
                     </div>
-                    <div className={"flex items-center flex-col w-full justify-center gap-1.5"}>
-                        <div className={"text-foreground text-sm font-medium outline-0"}>Бизнес</div>
-                        <div className={"flex flex-col gap-1"}>
-                            <Link className={"text-foreground-secondary text-xs font-medium hover:text-foreground"}
-                                  href={"/leads"}>Лиды</Link>
-                            <Link className={"text-foreground-secondary text-xs font-medium hover:text-foreground"}
-                                  href={"/clients"}>Клиенты</Link>
-                            <Link className={"text-foreground-secondary text-xs font-medium hover:text-foreground"}
-                                  href={"/deals"}>Сделки</Link>
-                        </div>
-                    </div>
-                    <div className={"flex items-center flex-col w-full justify-center gap-1.5"}>
-                        <div className={"text-foreground text-sm font-medium outline-0"}>Продуктивность</div>
-                        <div>
-                            <div className={"flex flex-col gap-1"}>
-                                <Link className={"text-foreground-secondary text-xs font-medium hover:text-foreground"}
-                                      href={"/tasks"}>Задачи</Link>
-                            </div>
-                        </div>
-                    </div>
-                    <div className={"flex items-center flex-col w-full justify-center gap-1.5"}>
-                        <div className={"text-foreground text-sm font-medium outline-0"}>Настройки</div>
-                        <div className={"flex flex-col gap-1"}>
-                            <Link className={"text-foreground-secondary text-xs font-medium hover:text-foreground"}
-                                  href={"/settings"}>Настройки</Link>
-                        </div>
-                    </div>
-                </nav>
-                <div className="text-xs text-muted-foreground text-center">
-                    © {new Date().getFullYear()} Event Sourcing CRM. All rights reserved.
                 </div>
+                <div className={"flex items-start justify-center gap-3.5 flex-col"}>
+                    <div className={"text-foreground text-3.5 font-medium"}>Коммерция</div>
+                    <div className={"flex flex-col gap-2"}>
+                        <Link className={"text-[14px] text-foreground-secondary hover:text-foreground"} href={"/"}>Сделки</Link>
+                        <Link className={"text-[14px] text-foreground-secondary hover:text-foreground"} href={"/"}>Лиды</Link>
+                        <Link className={"text-[14px] text-foreground-secondary hover:text-foreground"} href={"/"}>Клиенты</Link>
+                    </div>
+                </div>
+                <div className={"flex items-start justify-center gap-3.5 flex-col"}>
+                    <div className={"text-foreground text-3.5 font-medium"}>Задачи и аналитика</div>
+                    <div className={"flex flex-col gap-2"}>
+                        <Link className={"text-[14px] text-foreground-secondary hover:text-foreground"} href={"/"}>Аналитика</Link>
+                        <Link className={"text-[14px] text-foreground-secondary hover:text-foreground"} href={"/"}>Задачи</Link>
+                    </div>
+                </div>
+            </div>
+            <div className="text-3.5 text-foreground">
+                © {new Date().getFullYear()} CRM v0.1. Все права защищены.
             </div>
         </footer>
     )
